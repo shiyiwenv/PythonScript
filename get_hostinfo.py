037b5c93884716280f1480935a478d042f4465bc
shiyiwen <shiyiwen@sunteng.com>
2018-12-14 17:48:44 +0800

    收集主机信息，并保存至redis

--- get_hostinfo.py
+++ get_hostinfo.py
@@ -1,122 +1,130 @@
-#!/usr/bin/env python
-#encoding: utf-8
+# coding=utf-8
+import json
+import socket
+import struct
+import psutil
+import uuid,time,os
+import redis
 
-'''
-收集主机的信息：
-主机名称、IP、系统版本、服务器厂商、型号、序列号、CPU信息、内存信息
-'''
+class Redis(object):
+    def __init__(self,ip,password,port=6379,db=1):
+        try:
+            self.r = redis.Redis(host=ip, password=password, port=port, db=db)
+        except Exception as e:
+            print('redis连接失败，错误信息%s'%e)
 
-from subprocess import Popen, PIPE
-import os,sys
+    def str_set(self,k,v,time=None): #time默认失效时间
+        self.r.set(k,v,time)
 
-def getIfconfig():
-    p = Popen(['ifconfig'], stdout = PIPE)
-    data = p.stdout.read().split('\n\n')
-    return [i for i in data if i and not i.startswith('lo')]
+class CpuCollector(object):
+    @staticmethod
+    def get_base_info():
+        info = {
+            "cpu_count": psutil.cpu_count(),  # cpu物理核心数
+            "cpu_freq": psutil.cpu_freq().max  # cpu最高频率
+        }
+        return info
 
-''' 获取 dmidecode 命令的输出 '''
-def getDmi():
-    p = Popen(['dmidecode'], stdout = PIPE)
-    data = p.stdout.read()
-    return data
+class DiskCollector(object):
+    def __init__(self):
+        self.disk_part = []
 
-def parseIfconfig(data):
-    dic = {}
-    for devs in data:
-        lines = devs.split('\n')
-        devname = lines[0].split()[0]
-        #macaddr = lines[2].split()[1]
-        ipaddr  = lines[1].split()[1]
-        dic[devname] = [ipaddr]
-    return dic
+    def get_base_info(self):
+        self.disk_part = map(lambda x: dict(x._asdict()), psutil.disk_partitions())
+        info = {
+            "disk_part": self.disk_part,
+            "disk_usage": map(lambda x: dict(psutil.disk_usage(x['mountpoint'])._asdict()), self.disk_part)
+        }
+        return info
 
-''' 根据空行分段落 返回段落列表'''
-def parseData(data):
-    parsed_data = []
-    new_line = ''
-    data = [i for i in data.split('\n') if i]
-    for line in data:
-        if line[0].strip():
-            parsed_data.append(new_line)
-            new_line = line + '\n'
+class MemCollector(object):
+    @staticmethod
+    def get_base_info():
+        info = {
+            "phy_mem": dict(psutil.virtual_memory()._asdict()),
+            "swap_mem": dict(psutil.swap_memory()._asdict())
+        }
+        return info
+
+class NetWorkCollector(object):
+    def __init__(self):
+        self.inner_ips = {
+            24: ip_to_int('10.255.255.255') >> 24,
+            20: ip_to_int('172.31.255.255') >> 20,
+            16: ip_to_int('192.168.255.255') >> 16
+        }
+
+
+    def _is_inner_ip(self, ip):
+        ip_int = ip_to_int(ip)
+        for bit, val in self.inner_ips.iteritems():
+            if (ip_int >> bit) == val:
+                return True
+        return False
+
+    def _get_ip_type(self, ip):
+        if self._is_inner_ip(ip):
+            return 'inner_net'
+        elif ip in ('127.0.0.1',):
+            return 'lo_net'
         else:
-            new_line += line + '\n'
-    parsed_data.append(new_line)
-    return [i for i in parsed_data if i]
+            return 'outer_net'
 
-''' 根据输入的dmi段落数据 分析出指定参数 '''
-def parseDmi(parsed_data):
-    dic = {}
-    parsed_data = [i for i in parsed_data if i.startswith('System Information')]
-    parsed_data = [i for i in parsed_data[0].split('\n')[1:] if i]
-    dmi_dic = dict([i.strip().split(':') for i in parsed_data])
-    dic['vender'] = dmi_dic['Manufacturer'].strip()
-    dic['product'] = dmi_dic['Product Name'].strip()
-    dic['sn'] = dmi_dic['Serial Number'].strip()
-    return dic
+    def get_base_info(self):
+        eth_info = {
+            'inner_net': [],
+            'outer_net': [],
+            'lo_net': []
+        }
+        if_addr = psutil.net_if_addrs()
+        for eth, net in if_addr.iteritems():
+            ip = None
+            for n in net:
+                if n.family == 2 and n.address:
+                    ip = n.address
+                    break
+            else:
+                continue
+            if ip is None:
+                continue
+            ip_type = self._get_ip_type(ip)
+            eth_info[ip_type].append({'eth:': eth.decode('gbk'), 'ip_addr': ip})
+        return eth_info
 
-''' 获取Linux系统主机名称 '''
-def getHostname():
-    p = Popen(['hostname'], stdout = PIPE)
-    hostname = p.stdout.read().split('.')[0]
-    return {'hostname':hostname}
+def ip_to_int(ip):
+    return socket.ntohl(struct.unpack("I", socket.inet_aton(ip))[0])
 
-''' 获取CPU的型号和CPU的核心数 '''
-def getCpu():
-    num = 0
-    with open('/proc/cpuinfo') as fd:
-        for line in fd:
-            if line.startswith('processor'):
-                num += 1
-            if line.startswith('model name'):
-                cpu_model = line.split(':')[1].strip().split()
-                cpu_model = cpu_model[0] + ' ' + cpu_model[2]  + ' ' + cpu_model[-1]
-    return {'cpu_num':num, 'cpu_model':cpu_model}
 
-''' 获取Linux系统的总物理内存 '''
-def getMemory():
-    with open('/proc/meminfo') as fd:
-        for line in fd:
-            if line.startswith('MemTotal'):
-                mem = int(line.split()[1].strip())
-                break
-    mem = '%.f' % (mem / (1024.0*1024)) + 'GB'
-    return {'Memory':mem}
+def int_to_ip(int_ip):
+    return socket.inet_ntoa(struct.pack('I', socket.htonl(int_ip)))
 
-''' 获取Linux系统的版本信息 '''
-def getOsVersion():
-    with open('/etc/redhat-release') as fd:
-        for line in fd:
-            osver = line.strip()
-            break
-    return {'osver':osver}
+def uuidConllector():
+    uuidfile = '/etc/sysinfo_uuid'
+    if os.path.exists(uuidfile):
+        with open(uuidfile) as f:
+            return f.read()
+    else:
+        uuid = str(uuid.uuid4()).replace("-","")
+        with open(uuidfile,'w') as f:
+            f.write(uuid)
+            return uuid
 
-class Mergdic(object):
-    def __init__(self,dicname,soucedic):
-    self.merg = soucedic.update(dicname)
-    return self.merg
+def Total():
+    date = time.strftime("%Y-%m-%d", time.localtime())
+    total = {}
+    total.update(CpuCollector().get_base_info())
+    total.update(MemCollector().get_base_info())
+    total.update(DiskCollector().get_base_info())
+    total.update(NetWorkCollector().get_base_info())
 
-def Sysinfo():
-    sysinfo = {}
-    data = getIfconfig()
-    data_dmi = getDmi()
-    parsed_data_dmi = parseData(data_dmi)
+    total_info = {
+        'time': date, 'collect': total
+        }
 
-    dmi = parseDmi(parsed_data_dmi)
-    ip = parseIfconfig(data)
-    hostname = getHostname()
-    cpu = getCpu()
-    mem = getMemory()
-    osver = getOsVersion()
-    
-    sysinfo.update(ip)
-    sysinfo.update(dmi)
-    sysinfo.update(hostname)
-    sysinfo.update(cpu)
-    sysinfo.update(mem)
-    sysinfo.update(osver)
-    return sysinfo
-    
-    
+    return json.dumps(total_info)
+
 if __name__ == '__main__':
-   print Sysinfo()
+    uuid = uuidConllector()
+    jsonStr = Total()
+    r = Redis('192.168.0.79',password='',port=7777)
+    r.str_set(uuid,jsonStr)
